71c79b355e369510f2de0425562d63d93c21d9c1
Priyanshu <ps3556911@gmail.com>
2022-10-25 13:50:28 +0530

    Day-2 changes added

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(),
+      appBar: AppBar(
+        title: Text("AppBar"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to FLUTTER"),
+        ),
+      ),
+    );
+  }
+}
