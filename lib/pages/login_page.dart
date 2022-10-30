bfb7f915087c0032b89fa521d16ef957b86c7699
Priyanshu <ps3556911@gmail.com>
2022-10-30 13:11:11 +0530

    Day-5 Complete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalouge/utils/routes.dart';
 import 'package:gap/gap.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -11,50 +12,55 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
-        child: Center(
-            child: Column(
-          children: [
-            Gap(100),
-            SizedBox(
-              height: 200,
-              width: 200,
-              child: Image.asset(
-                "assets/images/login_image.png",
-                fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Gap(100),
+              SizedBox(
+                height: 200,
+                // width: 200,
+                child: Image.asset(
+                  "assets/images/login_image.png",
+                  alignment: Alignment.center,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-            const Gap(20),
-            const Text(
-              "Welcome To Login Screen!",
-              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-            ),
-            const Gap(20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 50),
-              child: Column(
-                children: [
-                  TextFormField(
-                    textCapitalization: TextCapitalization.words,
-                    decoration: const InputDecoration(
-                        hintText: "Enter Username", labelText: "Username"),
-                  ),
-                  TextFormField(
-                    obscureText: true,
-                    decoration: const InputDecoration(
-                        hintText: "Enter Password", labelText: "Password"),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text("Login"),
-                    style: TextButton.styleFrom(),
-                  )
-                ],
+              const Gap(20),
+              const Text(
+                "Welcome To Login Screen!",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
               ),
-            )
-          ],
-        )));
+              const Gap(20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 50),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      textCapitalization: TextCapitalization.words,
+                      decoration: const InputDecoration(
+                          hintText: "Enter Username", labelText: "Username"),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          hintText: "Enter Password", labelText: "Password"),
+                    ),
+                    Gap(30),
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyle(fontSize: 17),
+                      ),
+                      style: TextButton.styleFrom(minimumSize: Size(150, 40)),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
---
+++ lib/utils/routes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyRoutes {
+  static String loginRoute = "/LoginPage";
+  static String homeRoute = "/home";
+}
